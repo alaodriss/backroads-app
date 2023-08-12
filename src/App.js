066d902790450f91b432081f6b00c3cb8f40cd66
alaodriss <alaodriss@gmail.com>
2023-08-12 16:12:46 +0200

    add data for tours and footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
+import Tours from "./components/Tours"
 import Footer from "./components/Footer";
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
   <Hero />
   <About />
   <Services />
+  <Tours />
   <Footer />
 
   </>  
